0004292028dd259dca6da0314e012513cd5726dc
Amir Rahi <amirrahi29@gmail.com>
2022-10-06 15:37:00 +0530

    header issue resolved in middleware

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -6,16 +6,16 @@ const verifyToken = async(req,res,next)=>{
     const token = req.body.token || req.query.token || req.headers["authorization"];
     if(!token){
         res.status(200).send({success:false, msg:"A token is required for authentication!"});
+    }else{
+        try {
+            const decode = jwt.verify(token,config.secret_jwt);
+            req.user = decode;
+        } catch (error) {
+            res.status(400).send({success:false,msg:"Invalid token!"});
+        }
+    
+        return next();
     }
-
-    try {
-        const decode = jwt.verify(token,config.secret_jwt);
-        req.user = decode;
-    } catch (error) {
-        res.status(400).send({success:false,msg:"Invalid token!"});
-    }
-
-    return next();
 }
 
 module.exports = verifyToken;
